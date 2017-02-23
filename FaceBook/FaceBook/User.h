e57b60843a756ac1ccef83bda6b2368e251c9668
Mai Hoshino <insomnia.vov@gmail.com>
2017-02-22 21:20:29 -0800

    Delete profile files and transplant the contents to User files

--- FaceBook/FaceBook/User.h
+++ FaceBook/FaceBook/User.h
@@ -1,5 +1,5 @@
 //
-//  User.h
+//  Profile.h
 //  Linkedin
 //
 //  Created by Alireza Davoodi on 2017-02-21.
@@ -10,4 +10,22 @@
 
 @interface User : NSObject
 
+//properties
+
+@property (strong, nonatomic) NSString* firstname;
+@property (strong, nonatomic) NSString* lastname;
+@property (strong, nonatomic) NSString* address;
+@property int age;
+@property (strong, nonatomic) NSString* phoneNumber;
+@property (strong, nonatomic) NSString* profilePicURL;
+
+//constructors
+-(id) initWithFirstname:(NSString*)firstname lastname:(NSString*)lastname address:(NSString*)address age:(int)age phoneNumber:(NSString*)phoneNumber profilePicURL:(NSString*)profilePicURL;
+
+//methods (APIs)
+-(void) editProfileWithNewfirstname:(NSString*)newFirstname newLastname:(NSString*)newLastname newAddress:(NSString*)newAddress newAge:(int)newAge newPhoneNumber:(NSString*)newPhoneNumber newProfilePicURL:(NSString*)newProfilePicURL;
+
+-(void) saveProfile;
+
+-(void) printProfile;
 @end
--- FaceBook/FaceBook/User.m
+++ FaceBook/FaceBook/User.m
@@ -1,5 +1,5 @@
 //
-//  User.m
+//  Profile.m
 //  Linkedin
 //
 //  Created by Alireza Davoodi on 2017-02-21.
@@ -9,5 +9,59 @@
 #import "User.h"
 
 @implementation User
+
+-(id) initWithFirstname:(NSString*)firstname lastname:(NSString*)lastname address:(NSString*)address age:(int)age phoneNumber:(NSString*)phoneNumber profilePicURL:(NSString*)profilePicURL
+{
+    //step1: memory allocation
+    self = [super init];
+    
+    //step2: properties initialization
+    if(self)
+    {
+        self.firstname = firstname;
+        self.lastname = lastname;
+        self.address = address;
+        self.age = age;
+        self.phoneNumber = phoneNumber;
+        
+        self.profilePicURL = profilePicURL;
+    }
+    
+    return self;
+}
+
+
+-(void) editProfileWithNewfirstname:(NSString*)newFirstname newLastname:(NSString*)newLastname newAddress:(NSString*)newAddress newAge:(int)newAge newPhoneNumber:(NSString*)newPhoneNumber newProfilePicURL:(NSString*)newProfilePicURL
+{
+    self.firstname = newFirstname;
+    self.lastname = newLastname;
+    self.address = newAddress;
+    
+    self.age = newAge;
+    self.phoneNumber = newPhoneNumber;
+    self.profilePicURL = newProfilePicURL;
+    
+    
+}
+
+-(void) saveProfile
+{
+    NSLog(@"\nAccount has been successfully updated!\n");
+}
+
+-(void) printProfile
+{
+    NSLog(@"\n==============================\n");
+    
+    NSLog(@"Firstname is %@\n", self.firstname);
+    NSLog(@"Lastname is %@\n", self.lastname);
+    
+    NSLog(@"Address is %@\n", self.address);
+    NSLog(@"Age is %d\n", self.age);
+    
+    NSLog(@"Phone# is %@\n", self.phoneNumber);
+    NSLog(@"My Profile Picture is located at this link %@\n", self.profilePicURL);
+    NSLog(@"\n==============================\n");
+}
 
 @end
